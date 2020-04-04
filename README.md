c1531160a952c97f0f0701cac9ea20b32eeb6a1d
ekosistema <46524536+ekosistema@users.noreply.github.com>
2020-04-04 11:48:07 +0200

    compila.sh updated
    Updated compila.sh and README. Now you can type both filenames in the line without editing the code.

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # compile-and-debug
 
-Compile and debug an .asm file making use of [Yasm](https://yasm.tortall.net/), [GCC](https://gcc.gnu.org/) and [KDbg](http://www.kdbg.org/).   
+Compile and debug an .asm file making use of [Yasm](https://yasm.tortall.net/), [GCC](https://gcc.gnu.org/) and [KDbg](http://www.kdbg.org/).
 
 ## Instructions:
 
-Just run ./compila.sh your_file (without extension)   
-Remember to replace source_file with the right filename (if any)   
+Just run ./compila.sh your_asm_file your_c_file (without extension)  
+You must type the name of the .asm file (first) and .c file (second) without extension
 
-Happy debugging...    
-   
+Happy debugging...
--- compila.sh
+++ compila.sh
@@ -1,17 +1,18 @@
 # Compile and debug an.asm file with kdbg
 
-# Just run ./compila.sh your_file (without extension)
+# Just run ./compila.sh your_asm_file your_c_file (without extension)
 
-if [ $# -eq 1 ]
+if [ $# -eq 2 ]
 
 
 then
 
     asmfile=$1
+    cfile=$2
 
     echo "Compiling..."
 
-    yasm -f elf64 -g dwarf2 $asmfile.asm; gcc -no-pie -o $asmfile source_file.c $asmfile.o 
+    yasm -f elf64 -g dwarf2 $asmfile.asm; gcc -no-pie -o $asmfile $cfile.c $asmfile.o 
 
     echo "Debugging..."
 
@@ -21,8 +22,8 @@ else
 
     echo "-------------------------------------------------------------"
     echo "Error"
-    echo "You must type the name of the .asm file (without extension)"
-    echo "Remember to replace source_file with the right filename (if any)"
+    echo "You must type the name of the .asm file (first) and .c file (second) without extension"
+    echo "Example: ./compila-c.sh your_asm_file your_c_file"
     echo "Try again..."
     echo "-------------------------------------------------------------"
 
